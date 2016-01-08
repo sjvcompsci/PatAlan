33eb42ed0ca86670b8a5e9f2b0533a146346914f
pwalther1 <pwalther2019777@sjvhs.com>
2016-01-08 09:12:34 -0500

    added coded

---
+++ OtterSetup/OtterSetup/PlayerEntity.cs
@@ -0,0 +1,51 @@
+﻿using Otter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtterSetup
+{
+    class PlayerEntity : Entity
+    {
+
+        /// <summary>
+        /// The current move speed of the Entity.
+        /// </summary>
+        public float MoveSpeed;
+
+        /// <summary>
+        /// The move speed for when the Entity is moving slowly.
+        /// </summary>
+        public const float MoveSpeedSlow = 5;
+        /// <summary>
+        /// The move speed for when the Entity is moving quickly.
+        /// </summary>
+        public const float MoveSpeedFast = 10;
+        public PlayerEntity(float x, float y) : base(x, y)
+        {
+
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+
+            if (Input.KeyPressed(Key.Right))
+            {
+                this.X++;
+            }
+            if (Input.KeyPressed(Key.Left))
+            {
+                this.X--;
+            }
+            if (Input.KeyPressed(Key.Up))
+            {
+                this.Y++;
+            }
+        }
+    }
+}
+
---
+++ OtterSetup/OtterSetup/Program.cs
@@ -0,0 +1,22 @@
+﻿using Otter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtterSetup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a game
+            var game = new Game("Game", 1000, 500, 60, false);
+        }
+
+
+
+    }
+}
+
